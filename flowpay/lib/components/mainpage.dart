d590e324d7b74995540c9467c9567fc0bfc55822
Himanshu Malviya <malviya.himansh@gmail.com>
2023-02-28 15:19:33 +0530

    modified ui

--- flowpay/lib/components/mainpage.dart
+++ flowpay/lib/components/mainpage.dart
@@ -1,5 +1,6 @@
 import 'package:flowpay/components/paymentdetails.dart';
 import 'package:flutter/material.dart';
+import 'bottomsheet.dart';
 
 class mainPage extends StatelessWidget {
   const mainPage({Key? key}) : super(key: key);
@@ -13,7 +14,15 @@ class mainPage extends StatelessWidget {
       margin: const EdgeInsets.all(10),
       padding: const EdgeInsets.all(20),
       decoration: BoxDecoration(
-          color: const Color(0xFF762CD5),
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 59, 3, 90),
+              Color.fromARGB(255, 170, 95, 255),
+            ],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+          ),
+          // color: const Color(0xFF762CD5),
           borderRadius: BorderRadius.circular(30)),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -96,223 +105,21 @@ class mainPage extends StatelessWidget {
                       width: 10,
                     ),
                     Container(
-                        padding: const EdgeInsets.all(7),
-                        decoration: BoxDecoration(
-                            color: Colors.black,
-                            borderRadius: BorderRadius.circular(50)),
-                        child: IconButton(
-                          iconSize: 30,
-                          icon: const Icon(Icons.qr_code_rounded),
+                      padding: const EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(50)),
+                      child: GestureDetector(
+                        onTap: () {
+                          bottomsheet(context);
+                        },
+                        child: Icon(
+                          Icons.qr_code_rounded,
+                          size: 30,
                           color: Colors.white,
-                          onPressed: () {
-                            showBottomSheet(
-                              backgroundColor: Colors.grey[900],
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(25),
-                                      topRight: Radius.circular(25))),
-                              context: context,
-                              builder: (BuildContext context) {
-                                return Container(
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.55,
-                                  padding: EdgeInsets.all(15),
-                                  child: Column(
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Spacer(),
-                                          IconButton(
-                                            onPressed: () {
-                                              Navigator.pop(context);
-                                            },
-                                            icon: Icon(
-                                              Icons.close_rounded,
-                                              size: 30,
-                                            ),
-                                            color: Colors.white,
-                                          )
-                                        ],
-                                      ),
-                                      Text(
-                                        "Choose a chain",
-                                        style: TextStyle(
-                                            fontSize: 22,
-                                            fontWeight: FontWeight.w400,
-                                            color: Colors.white),
-                                      ),
-                                      SizedBox(
-                                        height: 40,
-                                      ),
-                                      GestureDetector(
-                                        onTap: () {
-                                          Navigator.push(
-                                              context,
-                                              MaterialPageRoute(
-                                                  builder: (context) =>
-                                                      PaymentDetails(
-                                                          selectedButton: 'sol',
-                                                          mobileNum:
-                                                              '7339933495')));
-                                        },
-                                        child: Container(
-                                          padding: EdgeInsets.symmetric(
-                                              horizontal: 20, vertical: 15),
-                                          decoration: BoxDecoration(
-                                              border: Border.all(
-                                                  color: Colors.white,
-                                                  width: 1),
-                                              borderRadius:
-                                                  BorderRadius.circular(100)),
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            mainAxisSize: MainAxisSize.min,
-                                            children: [
-                                              Image.network(
-                                                "https://cryptologos.cc/logos/flow-flow-logo.png",
-                                                height: 30,
-                                              ),
-                                              SizedBox(
-                                                width: 10,
-                                              ),
-                                              Text(
-                                                "Flow",
-                                                style: TextStyle(
-                                                  fontSize: 22,
-                                                  color: Colors.white,
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 20,
-                                      ),
-                                      GestureDetector(
-                                        onTap: () {},
-                                        child: Container(
-                                          padding: EdgeInsets.symmetric(
-                                              horizontal: 20, vertical: 15),
-                                          decoration: BoxDecoration(
-                                              border: Border.all(
-                                                  color: Colors.white,
-                                                  width: 1),
-                                              borderRadius:
-                                                  BorderRadius.circular(100)),
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            mainAxisSize: MainAxisSize.min,
-                                            children: [
-                                              Image.network(
-                                                "https://blog.logomyway.com/wp-content/uploads/2021/11/Ethereum-logo.png",
-                                                height: 30,
-                                              ),
-                                              SizedBox(
-                                                width: 10,
-                                              ),
-                                              Text(
-                                                "Ethereum",
-                                                style: TextStyle(
-                                                  fontSize: 22,
-                                                  color: Colors.white,
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 20,
-                                      ),
-                                      GestureDetector(
-                                        onTap: () {},
-                                        child: Container(
-                                          padding: EdgeInsets.symmetric(
-                                              horizontal: 20, vertical: 15),
-                                          decoration: BoxDecoration(
-                                              border: Border.all(
-                                                  color: Colors.white,
-                                                  width: 1),
-                                              borderRadius:
-                                                  BorderRadius.circular(100)),
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            mainAxisSize: MainAxisSize.min,
-                                            children: [
-                                              Image.network(
-                                                "https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png",
-                                                height: 30,
-                                              ),
-                                              SizedBox(
-                                                width: 10,
-                                              ),
-                                              Text(
-                                                "Solana",
-                                                style: TextStyle(
-                                                  fontSize: 22,
-                                                  color: Colors.white,
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        height: 20,
-                                      ),
-                                      GestureDetector(
-                                        onTap: () {},
-                                        child: Container(
-                                          padding: EdgeInsets.symmetric(
-                                              horizontal: 20, vertical: 15),
-                                          decoration: BoxDecoration(
-                                              border: Border.all(
-                                                color: Colors.white,
-                                                width: 1,
-                                              ),
-                                              borderRadius:
-                                                  BorderRadius.circular(100)),
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            mainAxisSize: MainAxisSize.min,
-                                            children: [
-                                              Icon(
-                                                Icons.currency_rupee,
-                                                size: 25,
-                                                color: Colors.white,
-                                              ),
-                                              SizedBox(
-                                                width: 10,
-                                              ),
-                                              Text(
-                                                "Rupees",
-                                                style: TextStyle(
-                                                  fontSize: 22,
-                                                  color: Colors.white,
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              },
-                            );
-                          },
-                        )
-                        // Icon(
-                        //   Icons.qr_code_rounded,
-                        //   size: 30,
-                        //   color: Colors.white,
-                        // ),
-                        )
+                        ),
+                      ),
+                    )
                   ],
                 ),
               )
--- flowpay/lib/components/paymentdetails.dart
+++ flowpay/lib/components/paymentdetails.dart
@@ -1,6 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
+Future<dynamic> showQR(BuildContext context, String mobileNum, String chain) {
+  return showModalBottomSheet(
+    elevation: 20,
+    backgroundColor: Colors.grey[900],
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(25), topRight: Radius.circular(25))),
+    context: context,
+    builder: (BuildContext context) {
+      return Container(
+        padding: EdgeInsets.only(bottom: 50, top: 20, right: 20, left: 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Spacer(),
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.close_rounded,
+                    size: 30,
+                  ),
+                  color: Colors.white,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            PaymentDetails(selectedButton: chain, mobileNum: mobileNum),
+            SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
+
 class PaymentDetails extends StatelessWidget {
   final String mobileNum;
   final String selectedButton;
@@ -9,17 +51,36 @@ class PaymentDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(mobileNum + "@" + selectedButton),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          QrImage(
+            data: mobileNum + "@" + selectedButton,
+            version: QrVersions.auto,
+            size: 300,
+            gapless: false,
+            foregroundColor: Colors.white,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "UPI ID: " + mobileNum + "@" + selectedButton,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 22,
+            ),
+          ),
+          // Text(
+          //   "Scan this code to pay me",
+          //   style: TextStyle(
+          //     color: Colors.white,
+          //     fontSize: 22,
+          //   ),
+          // ),
+        ],
       ),
-      body: Center(
-          child: QrImage(
-        data: mobileNum + "@" + selectedButton,
-        version: QrVersions.auto,
-        size: 320,
-        gapless: false,
-      )),
     );
   }
 }
